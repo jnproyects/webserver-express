08382238aba7f99be0266f7474520465cc2bc89c
Juan M <judith1912black@gmail.com>
2019-01-29 13:55:16 -0500

    primer commit

---
+++ app-old.js
@@ -0,0 +1,24 @@
+
+const http = require('http');
+
+
+http.createServer( ( req, res ) => {
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    let salida = {
+
+        nombre: 'Juan Pablo',
+        edad: 30,
+        url: req.url
+
+    }
+
+    res.write(JSON.stringify(salida));
+    // res.write('Hola mundo');
+    res.end();
+
+})
+.listen(3000);
+
+console.log('Escuchando el puerto 3000');
